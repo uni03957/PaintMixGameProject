9e439578d5ac0cd0dda817de7915baed266f94dc
uni03957 <62983154+uni03957@users.noreply.github.com>
2025-07-05 15:16:33 +0900

    Update app.py
    app.py ver 1.2
    
    [변경 내용]
    docstring 추가 및 imageBuf에 대한 조건문 간략화

--- ColorRandomGenerationSimulator/app.py
+++ ColorRandomGenerationSimulator/app.py
@@ -3,6 +3,21 @@ import numpy as np
 import colorVisualize as cv
 import logistic as lg
 
+def buildLUT(cdf, size=256):
+    """빠른 탐색을 위한 LUT 테이블을 생성하는 함수."""
+    # 작은 numpy 배열 룩업테이블을 통해 빠른 탐색.
+    # 정적인 룩업테이블이고, 모바일 경량화를 위해 1바이트만큼 메모리에 유지.
+    # CDF의 비율에 맞게 색상 인덱스를 저장함.
+    lut = np.zeros(size, dtype=int)
+    for i in range(size):
+        # 0~1까지를 256단계로 나눈 임의 값.
+        # CDF의 임계점과 비교해 룩업테이블을 완성.
+        value = i / (size - 1)
+
+        # 0~6까지의 인덱스는 색상(빨강, 초록, 파랑, 마젠타, 노랑, 시안, 블랙). 기본 값은 0으로 설정.
+        lut[i] = next((index for index, threshold in enumerate(cdf) if value <= threshold), 0)
+    return lut
+
 st.sidebar.title("물감 랜덤 생성 입력값")
 
 x0 = st.slider("시드 값", min_value=0.35, max_value=0.48, value=0.35, step=0.005)
@@ -16,26 +31,11 @@ cdf = np.cumsum(colorRatios)
 imageBuf = None
 
 
-# 작은 numpy 배열 룩업테이블을 통해 빠른 탐색.
-# 정적인 룩업테이블이고, 모바일 경량화를 위해 1바이트만큼 메모리에 유지.
-# CDF의 비율에 맞게 색상 인덱스를 저장함.
-LUT_SIZE = 256
-LUT = np.zeros(LUT_SIZE, dtype = int)
-
-for i in range(LUT_SIZE):
-    # 0~1까지를 256단계로 나눈 임의 값.
-    # CDF의 임계점과 비교해 룩업테이블을 완성.
-    value = i / (LUT_SIZE - 1) 
-
-    # 0~6까지의 인덱스는 색상. 기본 값은 0으로 설정.
-    LUT[i] = next((index for index, threshold in enumerate(cdf) if value <= threshold), 0)
-
-
 
 if st.button("시뮬레이션 실행"):
     logSequence = lg.logisticMapList(r,x0)
-    lutSequence = lg.mappingLUT(logSequence, LUT)
-    fig = cv.toConvertRectangleGrid(cv.toColorGrid(lutSequence), lutSequence, r, x0)
+    lutSequence = lg.mappingLUT(logSequence, buildLUT())
+    fig = cv.createColorGridFigure(cv.toColorGrid(lutSequence), lutSequence, r, x0)
     cv.renderColorGrid(fig)
     imageBuf = cv.saveImage(fig)
 
@@ -43,7 +43,7 @@ if st.button("시뮬레이션 실행"):
 st.write(f"선택된 시드: {x0}, 선택된 파라미터 r: {r}")
 
 
-if imageBuf is not None:
+if imageBuf:
     st.download_button(
         
         label="이미지 다운로드",
