75efab46f55c7796dcb27225f48d691f62fa057c
uni03957 <62983154+uni03957@users.noreply.github.com>
2025-07-05 15:04:41 +0900

    Create app.py
    app.py ver 1.0

---
+++ ColorRandomGenerationSimulator/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+import colorVisualize as cv
+import logistic as lg
+
+st.sidebar.title("물감 랜덤 생성 입력값")
+
+x0 = st.slider("시드 값", min_value=0.35, max_value=0.48, value=0.35, step=0.005)
+r = st.slider("로지스틱 파라미터 r", min_value=3.7, max_value=4.0, value=3.7, step=0.01)
+
+# 색상 비율과 CDF.
+# 인덱스 순서: Red, Green, Blue, Magenta, Yellow, Cyan, Black.
+colorRatios = [0.25, 0.25, 0.25, 0.08, 0.08, 0.08, 0.01]
+cdf = np.cumsum(colorRatios)
+
+imageBuf = None
+
+
+# 작은 numpy 배열 룩업테이블을 통해 빠른 탐색.
+# 정적인 룩업테이블이고, 모바일 경량화를 위해 1바이트만큼 메모리에 유지.
+# CDF의 비율에 맞게 색상 인덱스를 저장함.
+LUT_SIZE = 256
+LUT = np.zeros(LUT_SIZE, dtype = int)
+
+for i in range(LUT_SIZE):
+    # 0~1까지를 256단계로 나눈 임의 값.
+    # CDF의 임계점과 비교해 룩업테이블을 완성.
+    value = i / (LUT_SIZE - 1) 
+
+    # 0~6까지의 인덱스는 색상.
+    for index, threshold in enumerate(cdf):
+        if value <= threshold:
+            LUT[i] = index
+            break
+
+
+if st.button("시뮬레이션 실행"):
+    logSequence = lg.logisticMapList(r,x0)
+    lutSequence = lg.mappingLUT(logSequence, LUT)
+    imageBuf = cv.showColorRectangleGrid(cv.toColorGrid(lutSequence), lutSequence, r, x0)
+    
+st.write(f"선택된 시드: {x0}, 선택된 파라미터 r: {r}")
+
+if imageBuf is not None:
+    st.download_button(
+        
+        label="이미지 다운로드",
+        data=imageBuf,
+        file_name=f"color_grid_x0_{x0}_r_{r}.png",
+        mime="image/png"
+    )
