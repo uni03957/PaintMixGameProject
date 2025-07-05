6d14638e08ec1979a2439d83fc6e5875ab9a2869
uni03957 <62983154+uni03957@users.noreply.github.com>
2025-07-05 22:57:49 +0900

    Update logistic.py
    logistic.py ver 1.4
    
    [변경 내용]
    app.py와 마찬가지로 한꺼번에 업데이트

--- ColorRandomGenerationSimulator/logistic.py
+++ ColorRandomGenerationSimulator/logistic.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def logisticMapList(r: float, x0: float, count = 128, burnInNum = 20) -> list[float]:
+def logisticMapList(r: float, x0: float, count = 128, burnInNum = 20) -> np.ndarray:
     """시드 x0으로 파라미터 r에 기반한 로지스틱 사상을 계산 후, 초반 수렴 구간을 제외하고, 결과 리스트를 계산하여 반환하는 함수."""
     # 시드로 로지스틱 맵을 계산해서, 수열 리스트로 저장 후 반환.
     x = x0
@@ -9,24 +9,20 @@ def logisticMapList(r: float, x0: float, count = 128, burnInNum = 20) -> list[fl
     for i in range(burnInNum):
         x = r * x * (1 - x)
 
-    sequence = []
+    sequence = np.empty(count, dtype=np.float64)
     for i in range(count):
         x = r * x * (1-x)
-        sequence.append(x)
+        sequence[i] = x
     return sequence
 
-def mappingLUT(sequence: list[float], lut:np.array) -> list[int]:
+def mappingLUT(sequence: np.ndarray, lut:np.ndarray) -> np.ndarray:
     """로지스틱 사상의 결과로 0~1의 값을 지닌 요소들의 리스트를 받아 0~6의 컬러 인덱스로 매핑하는 함수. 
     0: Red, 1: Green, 2: Blue, 3: Magenta, 4: Yellow, 5: Cyan, 6: Black.
     """
     # 0~1값을 나눠 저장한 LUT에 기반하여, 컬러 인덱스와 매핑.
-    colorSequence=[]
 
-    for value in sequence:
-        # 256단계로 양자화 - ChatGPT
-        lutIndex = int(value * 255)
-        # 0~255 범위로 안전하게 제한 - ChatGPT
-        lutIndex = min(max(lutIndex, 0),255) 
-        colorSequence.append(lut[lutIndex])
+    # sequence 양자화하여 0 ~ 255 값 인덱스로 변경
+    colorIndices = np.clip((sequence * 255).astype(int), 0, 255)
 
-    return colorSequence
+    # 벡터화 인덱싱
+    return lut[colorIndices] 
