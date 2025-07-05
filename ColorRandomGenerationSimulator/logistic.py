e4a2d914d8aad2af1cab8e1aa6060073c2850496
uni03957 <62983154+uni03957@users.noreply.github.com>
2025-07-05 15:24:13 +0900

    Update logistic.py
    logistic.py ver 1.3
    
    [변경 내용]
    다른 파일과의 호환성 검토

--- ColorRandomGenerationSimulator/logistic.py
+++ ColorRandomGenerationSimulator/logistic.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def logisticMapList(r: float, x0: float, count = 128, burnInNum = 20) -> list[float]:
     """시드 x0으로 파라미터 r에 기반한 로지스틱 사상을 계산 후, 초반 수렴 구간을 제외하고, 결과 리스트를 계산하여 반환하는 함수."""
-    # 시드로 로지스틱 맵을 200번 계산해서, 수열 리스트로 저장 후 반환.
+    # 시드로 로지스틱 맵을 계산해서, 수열 리스트로 저장 후 반환.
     x = x0
 
     # ChatGPT가 제안한 초반 수렴 구간 제외하고 반영.
