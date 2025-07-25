4c2747837d53e836b906a1f0054654e95409b7ce
uni03957 <62983154+uni03957@users.noreply.github.com>
2025-07-25 16:14:24 +0900

    Update logistic.py
    커밋 오류로 인한 누락 해결

--- ColorRandomGenerationSimulator/logistic.py
+++ ColorRandomGenerationSimulator/logistic.py
@@ -25,3 +25,4 @@ def mappingLUT(sequence: np.ndarray, lut:np.ndarray) -> np.ndarray:
     colorIndices = np.clip((sequence * 255).astype(int), 0, 255)
 
     # 벡터화 인덱싱
+    return lut[colorIndices] 
